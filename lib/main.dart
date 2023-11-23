feae0718953c60c7be8446e540f3c5fe49e2c3ae
Wesley Moy <wesley@alteredlabs.ai>
2023-11-23 14:12:41 -0500

    Split app impl by platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 import 'src/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  if (kIsWeb) {
+    runApp(const FriendlyWebApp());
+    return;
+  }
+
+  if (Platform.isMacOS) {
+    runApp(const FriendlyMacApp());
+    return;
+  }
+
+  runApp(const FriendlyIOSApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FriendlyIOSApp extends StatelessWidget {
+  const FriendlyIOSApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
-      title: 'Flutter Demo',
+      title: 'Friendly Scorer',
+      home: HomePage(),
+    );
+  }
+}
+
+class FriendlyMacApp extends StatelessWidget {
+  const FriendlyMacApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class FriendlyWebApp extends StatelessWidget {
+  const FriendlyWebApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
       home: HomePage(),
     );
   }

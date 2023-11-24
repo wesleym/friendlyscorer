223118aca2e2e1158b7f3bf66c8781c3da22f116
Wesley Moy <wesley@alteredlabs.ai>
2023-11-24 11:51:18 -0500

    Add Chip's default answers

--- lib/src/data/defaults.dart
+++ lib/src/data/defaults.dart
@@ -1,42 +1,72 @@
+import 'package:friendlyscorer/src/data/repository.dart';
+
 import '../player/palette.dart';
 import 'models.dart';
 
-final defaultAnswers = [
-  'Britney Spears',
-  'Charles Barkley',
-  'Chevy Chase',
-  'Eddie Murphy',
-  'Elon Musk',
-  'Hugh Jackman',
-  'Justin Timberlake',
-  'Lin-Manuel Miranda',
-  'Magnus Carlsen',
-  'Paul Giamatti',
-  'Paul Simon',
-  'Robert Downey Jr.',
-  'Ruth Gordon',
-  'Steve Martin',
-  'Taylor Lautner',
-  'Taylor Swift',
-  'Tina Fey',
-  'Tom Hanks',
-].map((s) => Answer(id: s, text: s)).toList();
+Answer answerFromName(String name) {
+  return Answer(id: name, text: name);
+}
 
-final defaultPlayers = [
-  'Brian',
-  'Chip',
-  'Kathy',
-  'Lex',
-  'Shelley',
-  'CarlGPT',
-].map((n) {
+final steveMartin = answerFromName('Steve Martin');
+final taylorSwift = answerFromName('Taylor Swift');
+final eddieMurphy = answerFromName('Eddie Murphy');
+final chevyChase = answerFromName('Chevy Chase');
+
+final defaultAnswers = [
+  ...[
+    'Britney Spears',
+    'Charles Barkley',
+  ].map(answerFromName),
+  chevyChase,
+  eddieMurphy,
+  ...[
+    'Elon Musk',
+    'Hugh Jackman',
+    'Justin Timberlake',
+    'Lin-Manuel Miranda',
+    'Magnus Carlsen',
+    'Paul Giamatti',
+    'Paul Simon',
+    'Robert Downey Jr.',
+    'Ruth Gordon',
+  ].map(answerFromName),
+  steveMartin,
+  answerFromName('Taylor Lautner'),
+  taylorSwift,
+  ...[
+    'Tina Fey',
+    'Tom Hanks',
+  ].map(answerFromName),
+].toList();
+
+Player fromName(String name) {
   final nextId = PlayerIdVendor().next();
   return Player(
     id: nextId.toString(),
-    name: n,
+    name: name,
     color: playerColors[nextId],
   );
-}).toList();
+}
+
+final chip = fromName('Chip');
+
+final defaultPlayers = [
+  fromName('Brian'),
+  chip,
+  ...[
+    'Kathy',
+    'Lex',
+    'Shelley',
+    'CarlGPT',
+  ].map(fromName),
+].toList();
+
+final defaultPlayerAnswerAssociations = [
+  PlayerAnswerAssociation(playerId: chip.id, answerId: steveMartin.id),
+  PlayerAnswerAssociation(playerId: chip.id, answerId: taylorSwift.id),
+  PlayerAnswerAssociation(playerId: chip.id, answerId: eddieMurphy.id),
+  PlayerAnswerAssociation(playerId: chip.id, answerId: chevyChase.id),
+];
 
 final defaultRules = [
   'Buck Henry',

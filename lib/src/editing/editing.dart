641aae507ace4d576d943eb93c0e0fb6c524ece9
Wesley Moy <wesley.moy@gmail.com>
2023-12-13 18:42:36 -0500

    Add explanation of editing provider

--- lib/src/editing/editing.dart
+++ lib/src/editing/editing.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 
+/// A widget that lets descendant widgets know whether they should render in
+/// "editing mode". Widgets may show controls to make themselves mutable, or
+/// they may show options for destructive actions that should usually be hidden.
 class EditingProvider extends InheritedWidget {
   final bool editing;
 

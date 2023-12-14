4abd6546aeea2f16081f8ba6c90874a656cd5450
Wesley Moy <wesley.moy@gmail.com>
2023-12-13 19:20:53 -0500

    Extract answer column

---
+++ lib/src/answer/column.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:friendlyscorer/src/answer/models.dart';
+import 'package:friendlyscorer/src/answer/new_answer.dart';
+import 'package:friendlyscorer/src/answer/repository.dart';
+import 'package:friendlyscorer/src/answer/tiles.dart';
+import 'package:friendlyscorer/src/answer/trash.dart';
+import 'package:friendlyscorer/src/editing/editing.dart';
+import 'package:friendlyscorer/src/platform/button.dart';
+import 'package:friendlyscorer/src/platform/icon_button.dart';
+import 'package:friendlyscorer/src/platform/icons.dart';
+import 'package:friendlyscorer/src/platform/modal.dart';
+
+class AnswerColumn extends StatelessWidget {
+  final AnswerRepository _answerRepository;
+
+  const AnswerColumn({
+    super.key,
+    required AnswerRepository answerRepository,
+  }) : _answerRepository = answerRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    final editing = EditingProvider.of(context);
+    Widget? clearButton;
+    void Function(String answerId)? onDelete;
+    final Widget bottomAnswer;
+    if (editing.editing) {
+      clearButton = PlatformButton(
+        onPressed: () => _onClearAnswers(context),
+        child: const Text(
+          'Clear',
+          style: TextStyle(color: CupertinoColors.destructiveRed),
+        ),
+      );
+      onDelete = _onDeleteAnswer;
+      bottomAnswer = TrashTile(onDeleteAnswer: _onDeleteAnswer);
+    } else {
+      bottomAnswer = NewInnerAnswerTile(
+        onAddAnswers: (candidates) {
+          for (final a in candidates) {
+            final answer = Answer(id: a, text: a);
+            _answerRepository.add(answer);
+          }
+        },
+      );
+    }
+
+    return StreamBuilder(
+      initialData: _answerRepository.answers,
+      stream: _answerRepository.answerStream,
+      builder: (context, snapshot) {
+        var answers = snapshot.data!;
+        var answerTiles = answers.map((s) {
+          return AnswerTile(
+            key: ValueKey(s.id),
+            answer: s,
+            onDelete: onDelete,
+          );
+        });
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PlatformIcon(
+                  PlatformIcons.answers,
+                  color: CupertinoColors.inactiveGray,
+                ),
+                if (clearButton != null) clearButton,
+              ],
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: SingleChildScrollView(
+                primary: true,
+                padding: const EdgeInsets.symmetric(horizontal: 48),
+                child: Wrap(children: [
+                  ...answerTiles,
+                  bottomAnswer,
+                ]),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _onClearAnswers(BuildContext context) async {
+    final delete = await presentPlatformDestructionConfirmation(
+      context: context,
+      title: const Text('Delete all answers'),
+    );
+    if (delete) {
+      _answerRepository.clear();
+    }
+  }
+
+  void _onDeleteAnswer(String answerId) => _answerRepository.remove(answerId);
+}
--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:friendlyscorer/src/answer/models.dart';
-import 'package:friendlyscorer/src/answer/new_answer.dart';
+import 'package:friendlyscorer/src/answer/column.dart';
 import 'package:friendlyscorer/src/answer/repository.dart';
-import 'package:friendlyscorer/src/answer/tiles.dart';
-import 'package:friendlyscorer/src/answer/trash.dart';
-import 'package:friendlyscorer/src/editing/editing.dart';
-import 'package:friendlyscorer/src/platform/button.dart';
-import 'package:friendlyscorer/src/platform/icon_button.dart';
-import 'package:friendlyscorer/src/platform/icons.dart';
-import 'package:friendlyscorer/src/platform/modal.dart';
 import 'package:friendlyscorer/src/player/column.dart';
 import 'package:friendlyscorer/src/player/repository.dart';
 import 'package:friendlyscorer/src/rule/column.dart';
@@ -37,31 +29,6 @@ class _HomePageBodyState extends State<HomePageBody> {
 
   @override
   Widget build(BuildContext context) {
-    final editing = EditingProvider.of(context);
-    Widget? clearButton;
-    void Function(String answerId)? onDelete;
-    final Widget bottomAnswer;
-    if (editing.editing) {
-      clearButton = PlatformButton(
-        onPressed: _onClearAnswers,
-        child: const Text(
-          'Clear',
-          style: TextStyle(color: CupertinoColors.destructiveRed),
-        ),
-      );
-      onDelete = _onDeleteAnswer;
-      bottomAnswer = TrashTile(onDeleteAnswer: _onDeleteAnswer);
-    } else {
-      bottomAnswer = NewInnerAnswerTile(
-        onAddAnswers: (candidates) {
-          for (final a in candidates) {
-            final answer = Answer(id: a, text: a);
-            _answerRepository.add(answer);
-          }
-        },
-      );
-    }
-
     return Stack(
       children: [
         SafeArea(
@@ -75,44 +42,7 @@ class _HomePageBodyState extends State<HomePageBody> {
                 child: PlayerColumn(playerRepository: _playerRepository),
               ),
               Expanded(
-                child: StreamBuilder(
-                    initialData: _answerRepository.answers,
-                    stream: _answerRepository.answerStream,
-                    builder: (context, snapshot) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              PlatformIcon(
-                                PlatformIcons.answers,
-                                color: CupertinoColors.inactiveGray,
-                              ),
-                              if (clearButton != null) clearButton,
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Expanded(
-                            child: SingleChildScrollView(
-                              primary: true,
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 48),
-                              child: Wrap(children: [
-                                ...snapshot.data!.map(
-                                  (s) => AnswerTile(
-                                    key: ValueKey(s.id),
-                                    answer: s,
-                                    onDelete: onDelete,
-                                  ),
-                                ),
-                                bottomAnswer,
-                              ]),
-                            ),
-                          ),
-                        ],
-                      );
-                    }),
+                child: AnswerColumn(answerRepository: _answerRepository),
               ),
               SizedBox(
                 width: 140,
@@ -124,16 +54,4 @@ class _HomePageBodyState extends State<HomePageBody> {
       ],
     );
   }
-
-  void _onClearAnswers() async {
-    final delete = await presentPlatformDestructionConfirmation(
-      context: context,
-      title: const Text('Delete all answers'),
-    );
-    if (delete) {
-      _answerRepository.clear();
-    }
-  }
-
-  void _onDeleteAnswer(String answerId) => _answerRepository.remove(answerId);
 }
